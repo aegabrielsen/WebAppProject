3821c4839accdeeca01e1fa5e5cc7a28c48a230d
Alexander Gabrielsen <aegabrielsen@gmail.com>
2024-09-17 18:37:22 -0400

    Add visits cookie

--- util/paths/home_page.py
+++ util/paths/home_page.py
@@ -2,5 +2,6 @@
 def home_page(request, handler):
     f = open("public/index.html", "rb")
     index = f.read()
-    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(index)}\r\nContent-Type: text/html; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\n{index.decode()}"
+    visits = int(request.cookies.get("visits", 0)) + 1
+    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(index)}\r\nContent-Type: text/html; charset=utf-8\r\nSet-Cookie: visits={visits}; Max-Age=3600\r\nX-Content-Type-Options: nosniff\r\n\r\n{index.decode()}"
     handler.request.sendall(response.encode())
